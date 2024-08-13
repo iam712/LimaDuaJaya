0d45f101c41e6fd8cd397cc1b64a554d24d2c68c
JevonCledwyn <jcledwyn@student.ciputra.ac.id>
2024-08-13 09:51:46 +0700

    add mmodal review

--- resources/views/admin/review/review.blade.php
+++ resources/views/admin/review/review.blade.php
@@ -143,18 +143,33 @@
                             <td>123@gmail.com</td>
                             <td>halo</td>
                             <td>
-                                <a href="#" class="btn btn-warning btn-sm">Edit</a>
-                                <a href="#" class="btn btn-danger btn-sm">Delete</a>
+                                <button class="btn btn-danger btn-sm" data-bs-toggle="modal"
+                                    data-bs-target="#deleteModal">Delete</button>
                             </td>
                         </tr>
-                        <!-- Additional rows as needed -->
+                        <!-- Repeat for other rows -->
                     </tbody>
                 </table>
-
-                <div class="text-end mt-3">
-                    <a href="#" class="btn btn-success">Add New Review</a>
-                </div>
             </div>
         </section>
+    </div>
+
+    <!-- Delete Review Modal -->
+    <div class="modal fade" id="deleteModal" tabindex="-1" aria-labelledby="deleteModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="deleteModalLabel">Delete Review</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    Are you sure you want to delete this review?
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
+                    <button type="button" class="btn btn-danger">Delete Review</button>
+                </div>
+            </div>
+        </div>
     </div>
 @endsection
