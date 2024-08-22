e05c3e0a907d11ac40984a366c9c42aaeed3ddd4
JevonCledwyn <jcledwyn@student.ciputra.ac.id>
2024-08-22 14:43:01 +0700

    change bg sign in

--- resources/views/signin.blade.php
+++ resources/views/signin.blade.php
@@ -30,51 +30,10 @@
             overflow: hidden;
         }
 
-        /* Snowflake Styles */
-        .snowflake {
-            position: absolute;
-            background: white;
-            border-radius: 50%;
-            top: -5vh;
-        }
-
         .btn-primary {
             background-color: rgb({{ $color4 }});
             border: none;
         }
-
-        @keyframes snowfall {
-            0% {
-                transform: translate3d(var(--left-ini), 0, 0);
-            }
-
-            100% {
-                transform: translate3d(var(--left-end), 110vh, 0);
-            }
-        }
-
-        /* Generated CSS for each snowflake */
-        @for ($i = 0; $i < 50; $i++)
-            .snowflake:nth-child({{ $i + 1 }}) {
-                --size: {{ rand(1, 5) * 0.2 }}vw;
-                --left-ini: {{ rand(-10, 10) }}vw;
-                --left-end: {{ rand(-10, 10) }}vw;
-                left: {{ rand(0, 100) }}vw;
-                width: var(--size);
-                height: var(--size);
-                animation: snowfall {{ 5 + rand(1, 10) }}s linear infinite;
-                animation-delay: -{{ rand(1, 10) }}s;
-            }
-        @endfor
-
-        /* Optional: Adding blur effect to every 6th snowflake */
-        @for ($i = 0; $i < 50; $i++)
-            @if (($i + 1) % 6 == 0)
-                .snowflake:nth-child({{ $i + 1 }}) {
-                    filter: blur(1px);
-                }
-            @endif
-        @endfor
 
         /* Cube Styles */
         .cube-container {
@@ -139,7 +98,7 @@
 
         /* Background and Text Color Styles */
         .bg-custom {
-            background-color: rgb({{ $color1 }});
+            background-color: rgb({{ $color5 }});
             color: rgb({{ $color2 }});
         }
 
@@ -195,17 +154,147 @@
                 height: 80px;
             }
         }
+
+        /* Animated Background Styles */
+        .area {
+            width: 100%;
+            height: 100vh;
+            position: absolute;
+            z-index: 0;
+        }
+
+        .circles {
+            position: absolute;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            overflow: hidden;
+        }
+
+        .circles li {
+            position: absolute;
+            display: block;
+            list-style: none;
+            width: 20px;
+            height: 20px;
+            background: rgba(255, 255, 255, 1);
+            /* Make fully opaque to ensure visibility */
+            /* Color of the circles */
+            animation: animate 25s linear infinite;
+            bottom: -150px;
+        }
+
+        .circles li:nth-child(1) {
+            left: 25%;
+            width: 80px;
+            height: 80px;
+            animation-delay: 0s;
+        }
+
+        .circles li:nth-child(2) {
+            left: 10%;
+            width: 20px;
+            height: 20px;
+            animation-delay: 2s;
+            animation-duration: 12s;
+        }
+
+        .circles li:nth-child(3) {
+            left: 70%;
+            width: 20px;
+            height: 20px;
+            animation-delay: 4s;
+        }
+
+        .circles li:nth-child(4) {
+            left: 40%;
+            width: 60px;
+            height: 60px;
+            animation-delay: 0s;
+            animation-duration: 18s;
+        }
+
+        .circles li:nth-child(5) {
+            left: 65%;
+            width: 20px;
+            height: 20px;
+            animation-delay: 0s;
+        }
+
+        .circles li:nth-child(6) {
+            left: 75%;
+            width: 110px;
+            height: 110px;
+            animation-delay: 3s;
+        }
+
+        .circles li:nth-child(7) {
+            left: 35%;
+            width: 150px;
+            height: 150px;
+            animation-delay: 7s;
+        }
+
+        .circles li:nth-child(8) {
+            left: 50%;
+            width: 25px;
+            height: 25px;
+            animation-delay: 15s;
+            animation-duration: 45s;
+        }
+
+        .circles li:nth-child(9) {
+            left: 20%;
+            width: 15px;
+            height: 15px;
+            animation-delay: 2s;
+            animation-duration: 35s;
+        }
+
+        .circles li:nth-child(10) {
+            left: 85%;
+            width: 150px;
+            height: 150px;
+            animation-delay: 0s;
+            animation-duration: 11s;
+        }
+
+        @keyframes animate {
+            0% {
+                transform: translateY(0) rotate(0deg);
+                opacity: 1;
+                border-radius: 0;
+            }
+
+            100% {
+                transform: translateY(-1000px) rotate(720deg);
+                opacity: 0;
+                border-radius: 50%;
+            }
+        }
     </style>
 
     <section>
         <div class="signin-page">
-            <!-- Snowflakes -->
-            @for ($i = 0; $i < 50; $i++)
-                <div class="snowflake"></div>
-            @endfor
+            <!-- Animated Background -->
+            <div class="area">
+                <ul class="circles">
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
+            </div>
 
             <!-- Sign In Form Content -->
-            <div class="container" style="font-family: 'LibreBaskerville', serif;">
+            <div class="container" style="font-family: 'LibreBaskerville', serif, z-index: 10;">
                 <div class="row justify-content-center">
                     <div class="col-lg-8 col-md-10 col-12">
                         <div class="bg-custom shadow rounded-3 p-4 p-md-5">
@@ -219,7 +308,7 @@
                                     <div class="form">
 
                                         <!-- Display error message -->
-                                        @if($errors->has('login_error'))
+                                        @if ($errors->has('login_error'))
                                             <div class="alert alert-danger">
                                                 {{ $errors->first('login_error') }}
                                             </div>
@@ -246,7 +335,7 @@
                                                 </div>
                                             </div>
 
-                                            <div class="col-12">
+                                            <div class="col-12" style="z-index: 10;">
                                                 <button type="submit" class="btn btn-danger w-100 mt-3">Sign In</button>
                                             </div>
                                         </form>
