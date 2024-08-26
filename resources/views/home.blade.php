44adef089825dc166513b8ca33e3a6619f63717a
JevonCledwyn <jcledwyn@student.ciputra.ac.id>
2024-08-26 08:31:18 +0700

    FDZGXFCHGJVYHBKUILNO;'P

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -495,7 +495,7 @@
         body {
             margin: auto;
             font-family: Arial, sans-serif;
-            background: linear-gradient(315deg, rgba(101, 0, 94, 1) 3%, rgba(60, 132, 206, 1) 38%, rgba(48, 238, 226, 1) 68%, rgba(255, 25, 25, 1) 98%);
+            background: linear-gradient(315deg, rgba(101, 0, 94, 1) 3%, rgba(255, 25, 25, 1) 38%, rgba(48, 238, 226, 1) 68%, rgba(255, 25, 25, 1) 98%);
             animation: gradient 15s ease infinite;
             background-size: 400% 400%;
             background-attachment: fixed;
