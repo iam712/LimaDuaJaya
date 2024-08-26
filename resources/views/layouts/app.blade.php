09b7355c89f95acaaa0cc7900e1ee047860d7de6
iam712 <94553464+iam712@users.noreply.github.com>
2024-08-26 10:17:23 +0700

    cr4fg

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Lima Dua Jaya - @yield('title')</title>
+    <link rel="icon" type="image/png" href="{{ asset('images/logo-square.png') }}" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
